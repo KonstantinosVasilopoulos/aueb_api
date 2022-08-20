cf8e9b93675c1baa3785e37eafe67283e4df4681
Konstantinos Vasilopoulos <costasvas34@protonmail.com>
2022-08-20 18:31:37 +0300

    created models

--- aueb_api/aueb_exams/models.py
+++ aueb_api/aueb_exams/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Exam(models.Model):
+    """ Class representing a single examination date. """
+    id = models.AutoField(primary_key=True, verbose_name='id', db_column='ex_id')
+    date = models.CharField(verbose_name='date', max_length=10, db_column='ex_date')
+    time = models.CharField(verbose_name='time', max_length=11, db_column='ex_time')
+    room = models.CharField(verbose_name='room', max_length=255, db_column='ex_room')
+    department = models.CharField(verbose_name='department', max_length=255, db_column='ex_department')
+    professor = models.CharField(verbose_name='professor', max_length=255, db_column='ex_professor', blank=True, null=True)
+    course = models.CharField(verbose_name='course', max_length=255, db_column='ex_course')
+    notes = models.TextField(verbose_name='notes', db_column='ex_notes')
+
+
+    def __str__(self) -> str:
+        return 'Exam(id: {}, date: {}, time: {}, room: {}, department: {}, professor: {}, course: {}, notes: {})'.format(
+            self.id,
+            self.date,
+            self.time,
+            self.room,
+            self.department,
+            self.professor,
+            self.course,
+            self.notes
+        )
+
+
+    class Meta:
+        db_table = 'exams'
