1c33be69ee1ce0f6d1547b39013e6bbb0d59ea13
Apipilikas <apipilikas@gmail.com>
2022-08-22 18:09:46 +0300

    added time validator

--- aueb_api/aueb_exams/models.py
+++ aueb_api/aueb_exams/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from aueb_exams.validators import exam_date_validator
+from aueb_exams.validators import exam_date_validator,exam_time_validator
 
 
 class Exam(models.Model):
     """ Class representing a single examination date. """
     id = models.AutoField(primary_key=True, verbose_name='id', db_column='ex_id')
     date = models.CharField(verbose_name='date', max_length=10, db_column='ex_date', validators=[exam_date_validator])
-    time = models.CharField(verbose_name='time', max_length=11, db_column='ex_time')
+    time = models.CharField(verbose_name='time', max_length=11, db_column='ex_time', validators=[exam_time_validator])
     room = models.CharField(verbose_name='room', max_length=255, db_column='ex_room')
     department = models.CharField(verbose_name='department', max_length=255, db_column='ex_department')
     professor = models.CharField(verbose_name='professor', max_length=255, db_column='ex_professor', blank=True, null=True)
--- aueb_api/aueb_exams/validators.py
+++ aueb_api/aueb_exams/validators.py
@@ -1,11 +1,15 @@
 from django.core.validators import RegexValidator
 
-# DDMMYYYY date format validator
+# DD/MM/YYYY date format validator
 exam_date_validator = RegexValidator(
-    regex=r'^([0-9][1-9]){2}[0-9]{4}$',
-    message='Date must be of "DDMMYY" format.',
+    regex=r'^([0-9][1-9]\/){2}[0-9]{4}$',
+    message='Date must be of "DD/MM/YYYY" format.',
     code='invalid_date_format'
 )
 
-# TODO: HH:MM-HH:MM time format validator
-exam_time_validator = None
+# HH:MM-HH:MM time format validator
+exam_time_validator = RegexValidator(
+    regex=r'^[0-9]{2}:[0-9]{2}-[0-9]{2}:[0-9]{2}$',
+    message='Time must be of "HH:MM-HH:MM" format.',
+    code='invalid_time_format'
+)
